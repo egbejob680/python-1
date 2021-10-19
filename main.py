da34dd70e40c482977324118c6700ad9ee24a98c
Egbe Job <84234792+egbejob680@users.noreply.github.com>
2021-10-19 16:13:47 -0700

    exercise 1

--- main.py
+++ main.py
@@ -1,15 +1,50 @@
-class phoneBook:
-    def __init__(self,phoneBook):
-        self.name = phoneBook
-        self.records ={}
-    def add(self,name,contact,address):
-        self.records[name] = [contact,address]
-    def getRecords(self,name):
-        if name in self.records:
-            return self.records[name]
-        else:
-            return None
-phone = phoneBook("phone")
-phone.add("abel","346747","wyydhh")
-print("abel",(phone.getRecords("abel")[1]))
+#printing numbers from 1-20
+for i in range(0,20):
+    i+=1
+    print(i)
+
+#odd numbers less than 100
+for i in range(0,100):
+    if i%2!=0:
+        print(i,"is odd")
+    elif i==100:
+        print("")
+    else:
+        print("")
+#even numbers less than 100
+
+for i in range(0,100):
+    if i%2==0:
+        print(i,"is even")
+
+    else:
+        print(" ")
+
+# multiplication table with 7
+num = 7
+for i in range(0,20):
+    result=num*i
+    print(num,"x",i,"=",result)
+
+# calculating hcf
+
+def hcf(x,y):
+    if x>y:
+        smaller = y
+    else:
+        smaller = x
+        for i in range (1,smaller+1):
+            if((x%i==0) and (y%i==0)):
+                hcf=i
+    return hcf
+num1 = int(input("enter first;"))
+num2 = int(input("enter sec"))
+print("the hcf between",num1 ,"and",num2,"is",hcf(num1,num2))
+
+
+
+
+
+
+
 
