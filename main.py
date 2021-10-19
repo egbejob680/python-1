5e807a3c1092bd3a732b605370187c6ed6675db9
Egbe Job <84234792+egbejob680@users.noreply.github.com>
2021-10-19 16:15:31 -0700

    exercise 2

--- main.py
+++ main.py
@@ -1,50 +1,53 @@
-#printing numbers from 1-20
-for i in range(0,20):
+#converting from dec to binary only
+def binary(n):
+    if n>1:
+        binary(n//2)
+        print(n%2,end="")
+dec = int(input("enter a value: "))
+binary(dec)
+
+#converting from dec to others like; bin,hex,oct
+
+dec = int(input("enter a value : "))
+print("the decimal of ",dec,"is:" )
+print(bin(dec),"in binary")
+print(oct(dec),"in octal")
+print(hex(dec),"in hexadecimal")
+
+
+# determining power of certain amount of terms
+terms = int(input("total term:"))
+result = list(map(lambda x:2**x,range(terms)))
+for i in range(terms):
+    print("2 ^",i,"is",result[i])
+
+
+
+#multiplication table
+num = int(input("enter a value: "))
+for i in range(0,11):
+    num+=1
     i+=1
-    print(i)
+    print(num,"x",i,"=",num*i)
 
-#odd numbers less than 100
-for i in range(0,100):
-    if i%2!=0:
-        print(i,"is odd")
-    elif i==100:
-        print("")
-    else:
-        print("")
-#even numbers less than 100
-
-for i in range(0,100):
-    if i%2==0:
-        print(i,"is even")
-
-    else:
-        print(" ")
-
-# multiplication table with 7
-num = 7
-for i in range(0,20):
-    result=num*i
-    print(num,"x",i,"=",result)
-
-# calculating hcf
-
-def hcf(x,y):
-    if x>y:
-        smaller = y
-    else:
-        smaller = x
-        for i in range (1,smaller+1):
-            if((x%i==0) and (y%i==0)):
-                hcf=i
-    return hcf
-num1 = int(input("enter first;"))
-num2 = int(input("enter sec"))
-print("the hcf between",num1 ,"and",num2,"is",hcf(num1,num2))
+# to determine which number is larger or smaller
 
 
+num = float(input("enter value: "))
+num1 = float(input("enter value: "))
+num2 = float(input("enter value: "))
+if num>=num1 and num>=num2:
+    print("the largest number is",num,"cause it's the largest")
+elif num1>=num and num1>=num2:
+    print("the largest is",num1,"cause it's the largest")
+else:
+    print("the largest is:",num2,"cause it's the largest")
 
-
-
-
+if num<num1 and num<num2:
+    print("the smallest is :",num)
+elif num1<num and num1<num2:
+    print("the smallest is:",num1)
+else:
+    print("the smallest is:",num2)
 
 
